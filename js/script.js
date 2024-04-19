51710e336d1b4464fc2de329539836232384eb76
Palmatt98 <palmierimatteo98@gmail.com>
2024-04-19 19:05:25 +0200

    milestone-1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// tampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+Vue.createApp({
+	data() {
+		return {
+			todos: [
+				{
+					done: false,
+					text: "Recruiting blog post",
+				},
+				{
+					done: true,
+					text: "Mobile app luaunch",
+				},
+				{
+					done: false,
+					text: "Interview John H.",
+				},
+				{	
+					done: true,
+					text: "Summit update to mobile",
+				},
+				{
+					done: false,
+					text: "Summit update to mobil storefronts ",
+				},
+				{
+					done: false,
+					text: "ciao sono pippo",
+				},
+			],
+		};
+	},
+}).mount(".container-js");
