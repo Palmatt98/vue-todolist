1dc4f47e450c46a070f7dcee66dff2627dd969fb
Palmatt98 <palmierimatteo98@gmail.com>
2024-04-21 01:01:02 +0200

    banus-1

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 // tampare all'interno di una lista, un item per ogni todo.
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+//BONUS 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista (cercate nella documentazione l'evento che vi serve per questo bonus)
 Vue.createApp({
 	data() {
 		return {
@@ -46,5 +47,15 @@ Vue.createApp({
 				this.newItemText = null;
 			}
 		},
+		enterClicked: function(){
+			if(this.newItemText != null){
+				const newTodo = {
+					done: false,
+					text: this.newItemText
+				}
+				this.todos.push(newTodo);
+				this.newItemText = null
+			}
+		}
 	},
 }).mount(".container-js");
