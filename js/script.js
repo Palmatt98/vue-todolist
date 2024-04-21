f153733efcbf285412c20ced2be3b36112f194a3
Palmatt98 <palmierimatteo98@gmail.com>
2024-04-21 03:36:54 +0200

    completed + bonus 4

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 //BONUS 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista (cercate nella documentazione l'evento che vi serve per questo bonus)
+//  cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente ed eventuale icona (se done era uguale a false, impostare true e viceversa)
 Vue.createApp({
 	data() {
 		return {
@@ -36,6 +37,11 @@ Vue.createApp({
 			newItemText: null,
 		};
 	},
+	created() {
+		const localTodos = localStorage.getItem("todos");
+		console.log(localTodos);
+		this.todos = localTodos !== null ? JSON.parse(localTodos) : this.todos;
+	},
 	methods: {
 		addToList: function () {
 			if (this.newItemText != null) {
@@ -44,18 +50,23 @@ Vue.createApp({
 					text: this.newItemText,
 				};
 				this.todos.push(newTodo);
+				localStorage.setItem("todos", JSON.stringify(this.todos))
 				this.newItemText = null;
 			}
 		},
-		enterClicked: function(){
-			if(this.newItemText != null){
-				const newTodo = {
-					done: false,
-					text: this.newItemText
-				}
-				this.todos.push(newTodo);
-				this.newItemText = null
+		changeDone: function (todo) {
+			if (todo.done) {
+				//entra  nel ramo (brach) vero solo se todo.done è vero.
+				todo.done = false;
+			} else {
+				todo.done = true;
 			}
-		}
+			localStorage.setItem("todos", JSON.stringify(this.todos))
+		},
+		removeTodo: function (index) {
+			this.todos.splice(index, 1);
+			localStorage.setItem("todos", JSON.stringify(this.todos))
+		},
+		
 	},
 }).mount(".container-js");
